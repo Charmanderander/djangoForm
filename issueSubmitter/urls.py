690a726fc55173c3303f66a00f1113b47c3cfdbb
jinhao <jinhao_@hotmail.com>
2017-06-25 15:03:55 +0800

    added bootstrap3

--- issueSubmitter/urls.py
+++ issueSubmitter/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/', views.index, name='index'),
-    url(r'^issues/', views.issues, name='issues'),
+    url(r'^open/', views.open, name='open'),
+    url(r'^closed/', views.closed, name='closed'),
+    url(r'^collection/', views.collection, name='collection'),
+    url(r'^submit/', views.submit, name='submit'),
 ]
--- issueSubmitter/views.py
+++ issueSubmitter/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.template import RequestContext
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,8 +8,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def open(request):
+    my_data_dictionary = {}
+    return render(request, 'index.html', my_data_dictionary)
 
-def issues(request):
-    return HttpResponse("Hello, world. You're at the issues index.")
+def closed(request):
+    return HttpResponse("You're at the closed page.")
+
+def collection(request):
+    return HttpResponse("You're at the collections page.")
+
+def submit(request):
+    return HttpResponse("You're at the submit page.")
