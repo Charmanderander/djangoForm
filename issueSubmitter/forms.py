a99ee156a9b72446e68365f10ce9e0b11b1b1423
jinhao <jinhao_@hotmail.com>
2017-06-25 16:30:02 +0800

    testing commit from atom

--- issueSubmitter/forms.py
+++ issueSubmitter/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class NameForm(forms.Form):
     title = forms.CharField(label='Title', max_length=100)
     description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Short description of the problem'}))
-    files = forms.CharField(label='File involved', widget=forms.TextInput(attrs={'placeholder': '(e.g CSV files)'}), max_length=100)
+    files = forms.CharField(label='Files involved', widget=forms.TextInput(attrs={'placeholder': '(e.g CSV files)'}), max_length=100)
