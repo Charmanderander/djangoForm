1659fa7008f4946d3310c9e08a6e56058fccdc25
jinhao <jinhao_@hotmail.com>
2017-06-25 16:01:53 +0800

    added in content for the pages

---
+++ issueSubmitter/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class NameForm(forms.Form):
+    title = forms.CharField(label='Title', max_length=100)
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Short description of the problem'}))
+    files = forms.CharField(label='File involved', widget=forms.TextInput(attrs={'placeholder': '(e.g CSV files)'}), max_length=100)
--- issueSubmitter/views.py
+++ issueSubmitter/views.py
@@ -2,21 +2,36 @@
 from __future__ import unicode_literals
 from django.template import RequestContext
 from django.shortcuts import render
-
-# Create your views here.
-
+from .forms import NameForm
 from django.http import HttpResponse
-
+# Create your views here.
 
 def open(request):
     my_data_dictionary = {}
-    return render(request, 'index.html', my_data_dictionary)
+    return render(request, 'open.html', my_data_dictionary)
 
 def closed(request):
-    return HttpResponse("You're at the closed page.")
+    my_data_dictionary = {}
+    return render(request, 'closed.html', my_data_dictionary)
 
 def collection(request):
-    return HttpResponse("You're at the collections page.")
+    my_data_dictionary = {}
+    return render(request, 'collection.html', my_data_dictionary)
 
 def submit(request):
-    return HttpResponse("You're at the submit page.")
+        # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'submit.html', {'form': form})
