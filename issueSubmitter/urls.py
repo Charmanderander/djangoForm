43a752083309963999ba28d9f6acb24ce761171c
jinhao <jinhao_@hotmail.com>
2017-06-26 17:01:12 +0800

    fixed the url dispatcher

--- issueSubmitter/urls.py
+++ issueSubmitter/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
-from django.views.generic.base import RedirectView
 
 from . import views
 
-urlpatterns = [
+patterns = ['open/','closed/','collection/','submit/']
 
+urlpatterns = [
     url(r'^open/', views.open, name='open'),
     url(r'^closed/', views.closed, name='closed'),
     url(r'^collection/', views.collection, name='collection'),
     url(r'^submit/', views.submit, name='submit'),
-    url(r'^.*$', RedirectView.as_view(pattern_name='open', permanent=False))
+    url(r'^$', views.open, name='open')
 ]
